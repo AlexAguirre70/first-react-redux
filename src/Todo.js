b3d8cdc3835e461f87b994976b0423b821bc0964
Alex Aguirre <teamaguirre@gmail.com>
2022-09-23 21:27:20 -0700

    corrected import error

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,3 @@
-import { iteratorSymbol } from 'immer/dist/internal'
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { addTodo, clearTodo } from './features/todoSlice'
